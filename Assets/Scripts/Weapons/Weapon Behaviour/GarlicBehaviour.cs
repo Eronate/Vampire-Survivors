bb4299ba3c43426649d2063d2131ca49e6faf951
Cristian <kitidudu@yahoo.com>
2023-05-02 16:19:01 +0300

    fixed garlic

--- Assets/Scripts/Weapons/Weapon Behaviour/GarlicBehaviour.cs
+++ Assets/Scripts/Weapons/Weapon Behaviour/GarlicBehaviour.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class GarlicBehaviour : MeleeWeaponBehaviour
 {
-    // Start is called before the first frame update
+    List<GameObject> markedEnemies;
     protected override void Start()
     {
         base.Start();
+        markedEnemies = new List<GameObject>();
     }
 
+    protected override void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("Enemy") && !markedEnemies.Contains(col.gameObject))
+        {
+            EnemyStats enemy = col.GetComponent<EnemyStats>();
+            enemy.TakeDamage(currentDamage);
+            markedEnemies.Add(col.gameObject); //marked so it doesn't take another tick of damage from this garlic 
+        }
+    }
 
 }
